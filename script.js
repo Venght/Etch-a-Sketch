c324d3b97868ec5d5d964a10a7f3f4f486fb0d05
Venght <bvenght@gmail.com>
2024-12-20 19:30:14 +0800

    The etch the sketch is done

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#pixel");
+let input;
 
-function inputGrid() {
-  let person = prompt("Please enter an amount from ");  
-  for (i = 0; i < 16 * 16; i++) {
+
+function inputGrid(size) {
+  container.textContent = "";
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+  for (i = 0; i < size * size; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
+
+    gridItem.addEventListener("mouseover", () => {
+      const rainbow = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"]
+      const randomColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+      gridItem.style.backgroundColor = randomColor;
+    });
+
     container.appendChild(gridItem);
   }
 }
 
-inputGrid()
+function gridAdjust() {
+  button.addEventListener("click", () => {
+    while (true) {
+      let input = prompt("Input a number between 1-100? ");
+      input = Number(input);
+
+      if (input >= 1 && input <= 100) {
+        inputGrid(input)
+        break;
+      } else {
+        alert("The value you input is not in range, input again?");
+        alert(input)
+      }
+    }
+    return input;
+  });
+}
+
+function reset() {
+  const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener('click', ()=>{
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((item) => {
+      item.style.backgroundColor = "white";
+    })
+  })
+}
+
+reset();
+gridAdjust();
+inputGrid(16);
